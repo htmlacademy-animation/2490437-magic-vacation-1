16594e54b5c0d3136e51aa52b43948a3de411f94
Yuliya Sokolova <sokolova.yua@sbermarket.ru>
2024-06-04 15:23:57 +0300

    Добавила комментарий

--- source/js/modules/full-page-scroll.js
+++ source/js/modules/full-page-scroll.js
@@ -55,6 +55,7 @@ export default class FullPageScroll {
   }
 
   changeVisibilityDisplay() {
+    // Задержка смены классов для показа экрана историй при выполнении анимации заполнения фона на экране призов
     if (this.screenElements[this.activeScreen].id === 'prizes') {
       setTimeout(() => {
         this.screenElements.forEach((screen) => {
